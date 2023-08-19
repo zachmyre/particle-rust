f43d29ac94c500853815a64e347aea223dfdba9b
Zach Myre <myrezach@live.com>
2023-08-19 15:56:21 -0500

    glow effect

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(clippy::unnecessary_wraps)]
+#[macro_use]
+extern crate lazy_static;
 extern crate rand;
 
 use ggez::{
@@ -12,16 +14,29 @@ use ggez::{
 use rand::{random, Rng};
 
 const MAX_PARTICLES: i32 = 500;
-const PARTICLE_RADIUS: f32 = 12.0;
-const COLORS: [Color; 7] = [
-    Color::BLACK,
-    Color::BLUE,
-    Color::CYAN,
-    Color::GREEN,
-    Color::RED,
-    Color::WHITE,
-    Color::YELLOW,
-];
+const PARTICLE_RADIUS: f32 = 5.0;
+// using lazy_static to generate colors (rust compiler fix)
+lazy_static! {
+    static ref NEON_GREEN: Color = Color::from([0.224, 1.0, 0.078, 1.0]);
+    static ref NEON_BLUE: Color = Color::from([0.098, 0.454, 0.827, 1.0]);
+    static ref NEON_PINK: Color = Color::from([1.0, 0.192, 0.333, 1.0]);
+    static ref NEON_YELLOW: Color = Color::from([1.0, 0.835, 0.0, 1.0]);
+    static ref NEON_CYAN: Color = Color::from([0.0, 0.659, 0.918, 1.0]);
+    static ref NEON_ORANGE: Color = Color::from([1.0, 0.341, 0.133, 1.0]);
+    static ref NEON_PURPLE: Color = Color::from([0.502, 0.0, 0.502, 1.0]);
+}
+
+lazy_static! {
+    static ref COLORS: [Color; 7] = [
+        *NEON_GREEN,
+        *NEON_BLUE,
+        *NEON_PINK,
+        *NEON_YELLOW,
+        *NEON_CYAN,
+        *NEON_ORANGE,
+        *NEON_PURPLE,
+    ];
+}
 
 const GRAVITY: f32 = 0.9;
 const ENERGY_LOSS: f32 = 0.8;
@@ -74,7 +89,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         for particle in &mut self.particles {
             // Apply gravity to the particle's velocity
-            particle.velocity.y += GRAVITY / GRAVITY - 0.1;
+            particle.velocity.y += GRAVITY;
 
             // particle.velocity.x += GRAVITY / GRAVITY - 0.1;
 
@@ -105,16 +120,28 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         // Create a new canvas to draw to.
-        let mut canvas = graphics::Canvas::from_frame(ctx, Color::from([0.1, 0.2, 0.3, 1.0]));
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
 
         for particle in &self.particles {
+            // Draw the "glow"
+            let glow_circle = graphics::Mesh::new_circle(
+                ctx,
+                graphics::DrawMode::fill(),
+                particle.position,
+                particle.radius * 1.2, // Larger than the actual particle
+                0.1,
+                Color::new(particle.color.r, particle.color.g, particle.color.b, 0.4), // Lower alpha for the glow
+            )?;
+            glow_circle.draw(&mut canvas, graphics::DrawParam::default());
+
+            // Draw the actual particle
             let circle = graphics::Mesh::new_circle(
                 ctx,
                 graphics::DrawMode::fill(),
                 particle.position,
                 particle.radius,
                 0.1,
-                particle.color,
+                particle.color, // Full alpha for the particle itself
             )?;
             circle.draw(&mut canvas, graphics::DrawParam::default());
         }
@@ -128,8 +155,10 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
 pub fn main() -> GameResult {
     let window_config = ggez::conf::WindowMode::default().dimensions(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    let cb = ggez::ContextBuilder::new("particles_with_gravity", "ggez").window_mode(window_config);
+    let window_setup = ggez::conf::WindowSetup::default().title("Zach's Particle System");
+    let cb = ggez::ContextBuilder::new("particles_with_gravity", "ggez")
+        .window_mode(window_config)
+        .window_setup(window_setup);
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
